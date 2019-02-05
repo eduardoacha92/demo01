fb7d1fa5d464e571c159767c2e077a7c22e4a9a5
Eduardo Acha <eduardoacha@Eduardos-MBP.hsd1.tx.comcast.net>
2019-02-04 22:10:58 -0600

    Updates

---
+++ precache-manifest.2e4903b17468b01c7e3214dc1d6d19ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo01/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b47b420e70f40dbd1e1",
+    "url": "/demo01/static/js/runtime~main.3b47b420.js"
+  },
+  {
+    "revision": "a75ac1e417b02e53e2b2",
+    "url": "/demo01/static/js/main.a75ac1e4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/demo01/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a75ac1e417b02e53e2b2",
+    "url": "/demo01/static/css/main.5fa30191.chunk.css"
+  },
+  {
+    "revision": "1bea790f8739c51c1e938e3a4f3f5adb",
+    "url": "/demo01/index.html"
+  }
+];
